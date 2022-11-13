8c1252fba69f97faaf021282fe371a3cbd5f8acd
Vaidehi Sinha <1032201688@mitwpu.edu.in>
2022-11-13 15:59:03 +0530

    About and Contact

---
+++ frontend/myapp/src/components/about_us/about1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from "../home/navbar"
+import Footer from '../home/footer';
+import About1 from './about';
+
+export default function About() {
+  return (
+    <div>
+        <Navbar/>
+        <About1/>
+        <Footer/>
+    </div>
+  );
+}
